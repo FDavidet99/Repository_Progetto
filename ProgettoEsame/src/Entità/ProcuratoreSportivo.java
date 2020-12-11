a517de5601966649d45a20b37954784a314107b8
FDavidet99 <75310826+FDavidet99@users.noreply.github.com>
2020-12-11 18:28:33 +0100

    Aggiunti Set e Get

--- ProgettoEsame/src/Entità/ProcuratoreSportivo.java
+++ ProgettoEsame/src/Entità/ProcuratoreSportivo.java
@@ -1,9 +1,55 @@
 package Entità;
+import java.util.Date; 
 
 public class ProcuratoreSportivo {
 	private String CodiceFiscale;
 	private String Nome;
 	private String Cognome;
+	private String Sesso;
 	private Date DataNascita;
+	private String Provincia;
+	private String CAP;
+	public String getCodiceFiscale() {
+		return CodiceFiscale;
+	}
+	public void setCodiceFiscale(String codiceFiscale) {
+		CodiceFiscale = codiceFiscale;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getCognome() {
+		return Cognome;
+	}
+	public void setCognome(String cognome) {
+		Cognome = cognome;
+	}
+	public String getSesso() {
+		return Sesso;
+	}
+	public void setSesso(String sesso) {
+		Sesso = sesso;
+	}
+	public Date getDataNascita() {
+		return DataNascita;
+	}
+	public void setDataNascita(Date dataNascita) {
+		DataNascita = dataNascita;
+	}
+	public String getProvincia() {
+		return Provincia;
+	}
+	public void setProvincia(String provincia) {
+		Provincia = provincia;
+	}
+	public String getCAP() {
+		return CAP;
+	}
+	public void setCAP(String cAP) {
+		CAP = cAP;
+	}
 
 }
