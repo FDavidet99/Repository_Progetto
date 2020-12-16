ee1bbba0076bd4357d0e01fef53f3bcb670524c7
FDavidet99 <75310826+FDavidet99@users.noreply.github.com>
2020-12-16 17:05:45 +0100

    Update MainClass.java

--- ProgettoEsame/src/Entita/MainClass.java
+++ ProgettoEsame/src/Entita/MainClass.java
@@ -14,7 +14,7 @@ public class MainClass {
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		//Persona p1 = new Persona("Alessandro", "Bonomo", LocalDate.of(2001, 3, 28),"Caserta");
-		Persona p2 = new Persona("Davide", "Ferreri",Sesso.M, LocalDate.of(1999, 2, 5),"Italia","Napoli","Forio");
+		Persona p2 = new Persona("Davide", "Ferreri",Sesso.M, LocalDate.of(1999, 2, 5),"Italia","Napoli","Lacco Ameno");
 		System.out.println(p2);
 		//System.out.println(p2.getCF());
 	}
