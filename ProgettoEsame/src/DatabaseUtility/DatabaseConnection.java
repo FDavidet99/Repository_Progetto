b30071c57f7c3f6a2b6cf970ac22e993ab8460b6
Alexbonny28 <abonomo2001@gmail.com>
2020-12-17 18:13:00 +0100

    hh

--- ProgettoEsame/src/DatabaseUtility/DatabaseConnection.java
+++ ProgettoEsame/src/DatabaseUtility/DatabaseConnection.java
@@ -3,9 +3,6 @@ package DatabaseUtility;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
 
 public class DatabaseConnection {
 	private static DatabaseConnection instance;
